a0bb03f905a84d9d58a38a27a3e2e9d111fd5373
M.C. Brouwer <matthijs@artandflywork.org>
2016-03-06 20:08:00 +0100

    fixed panel not loading on openshift

--- site.php
+++ site.php
@@ -4,20 +4,21 @@ $kirby = kirby();
 
 if (isset($_ENV['OPENSHIFT_DATA_DIR'])) {
 
-	$openshiftDataDir = getenv('OPENSHIFT_DATA_DIR');
-	
-	// get the changing data from the Openshift datadir
-	$kirby->roots->content  = $openshiftDataDir . DS . 'content';
-	$kirby->roots->thumbs   = $openshiftDataDir . DS . 'thumbs';
-	$kirby->roots->cache    = $openshiftDataDir . DS . 'cache';
-	$kirby->roots->avatars  = $openshiftDataDir . DS . 'avatars';
-	$kirby->roots->accounts = $openshiftDataDir . DS . 'accounts';
-	$kirby->roots->cache    = $openshiftDataDir . DS . 'cache';
+    $openshiftDataDir = getenv('OPENSHIFT_DATA_DIR');
+    
+    // get the changing data from the Openshift datadir
+    $kirby->roots->content  = $openshiftDataDir . DS . 'content';
+    $kirby->roots->thumbs   = $openshiftDataDir . DS . 'thumbs';
+    $kirby->roots->cache    = $openshiftDataDir . DS . 'cache';
+    $kirby->roots->avatars  = $openshiftDataDir . DS . 'avatars';
+    $kirby->roots->accounts = $openshiftDataDir . DS . 'accounts';
+    $kirby->roots->cache    = $openshiftDataDir . DS . 'cache';
 
-	// a custom openshift alias is added in .htaccess
-	// so this site works in regular env as well as openshift
-	$kirby->urls->content   = $kirby->urls()->index() . '/oscontent';
-	$kirby->urls->thumbs    = $kirby->urls()->index() . '/osthumbs';
-	$kirby->urls->avatars   = $kirby->urls()->index() . '/osavatars';
-	
+    // the openshift content dir is available through a soft link
+    // wich is created at deployment by the script .openshift/action_hooks/deploy
+    // this is so the regular content dir is still usable in dev environment
+    $kirby->urls->content   = $kirby->urls()->index() . '/oscontent';
+    $kirby->urls->thumbs    = $kirby->urls()->index() . '/osthumbs';
+    $kirby->urls->avatars   = $kirby->urls()->index() . '/osavatars';
+    
 }
