5c7fae57ead4a561a647fd662194e85539dd88bd
M.C. Brouwer <matthijs@artandflywork.org>
2016-12-18 21:01:00 +0100

    added JS to open all external links in new tab

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,6 +4,13 @@
 	  <?php echo $site->copyright()->kirbytext() ?>
 	  <p class="colophon">Site made with <b>&hearts;</b> and <a href="http://getkirby.com">Kirby</a></p>
 	</footer>
-
+	<script>
+		var a = document.querySelectorAll('a');
+		for (var i = 0; i < a.length; i++) {
+			if (a[i].host !== window.location.hostname) {
+				a[i].setAttribute('target', '_blank');
+		 	}
+		}
+	</script>
 </body>
 </html>
